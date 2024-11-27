fdbe65250ddd26c0ef7905c844c2268ed99c1229
sushantkumarsingh <singhji1359@gmail.com>
2024-11-27 21:45:14 +0530

    login validators added

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,11 +3,12 @@ import { registrationUser } from "../controller/authentication.js";
 import { loginUserAuth } from "../controller/loginUser.js";
 import { userAuthValidatorsSchema } from "../validators/userAuthValidators.js"
 import { validate } from "../middlewares/authMiddlewares.js"
+import { loginValidatorSchema } from "../validators/loginValidatores.js";
 
 const route = express.Router();
 
 route.post("/registration", validate(userAuthValidatorsSchema), registrationUser)
-route.post("/login", loginUserAuth)
+route.post("/login",validate(loginValidatorSchema), loginUserAuth)
 
 
 export default route;
